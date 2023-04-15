724290a520095762cf1055103813c04b400bafd6
emilianoprioli <emilianoprioli.ep@gmail.com>
2023-04-15 09:58:38 -0300

    front

--- PI-Videogames-main/client/src/App.js
+++ PI-Videogames-main/client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+
+import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Videogames</h1>
+      
     </div>
   );
 }
---
+++ PI-Videogames-main/client/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom"
+
+const LandingPage = () => {
+    return(
+        <div>
+            <Link to="/"><button>Go to home page</button></Link>
+        </div>
+    )
+}
+
+export default LandingPage;
